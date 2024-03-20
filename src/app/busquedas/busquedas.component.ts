f30a2874a85503525bdaf936559645af52cd140b
RAMOS LAZCANO JOSE ALBERTO <JOSE.LAZCANO@inegi.org.mx>
2024-03-20 16:00:28 -0600

    stuck web

---
+++ src/app/busquedas/busquedas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busquedas',
+  standalone: true,
+  imports: [],
+  templateUrl: './busquedas.component.html',
+  styleUrl: './busquedas.component.css'
+})
+export class BusquedasComponent {
+
+}
---
+++ src/app/fecha-y-hora/fecha-y-hora.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fecha-y-hora',
+  standalone: true,
+  imports: [],
+  templateUrl: './fecha-y-hora.component.html',
+  styleUrl: './fecha-y-hora.component.css'
+})
+export class FechaYHoraComponent {
+
+}
