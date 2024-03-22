dbb11b7b84a0d821f0cc60e60e706e5e60c6c337
Urrix <mauribarrien@gmail.com>
2024-03-22 15:32:18 -0600

    ApiMaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule,MatMenuModule],
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
