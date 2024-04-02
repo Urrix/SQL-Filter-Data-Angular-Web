1838612b61f2c1823ba925a6bf99f1026cc36123
Urrix <mauribarrien@gmail.com>
2024-04-02 16:15:58 -0600

    git table filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 interface State {
-  name: string;
   abbreviation: string;
+  name: string;
   population: number;
   area: number;
 }
