47f0458011eb28d0410f6c08cc511056ceed0bf0
Urrix <mauribarrien@gmail.com>
2024-04-02 14:32:29 -0600

    SlideNav menu

--- src/app/palabras/palabras.component.ts
+++ src/app/palabras/palabras.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import { ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-palabras',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,MatSidenavModule, MatButtonModule,MatCardModule,MatDividerModule, MatButtonModule, MatProgressBarModule],
   templateUrl: './palabras.component.html',
   styleUrl: './palabras.component.css'
 })
-export class PalabrasComponent {
+export class AppComponent {
+  @ViewChild('drawer') drawer!: ElementRef<HTMLElement>;
+  title = 'ScrumRestaurante';
+  longText = `..............................................................................
+  .......................`;
+  showFiller: boolean = false;
 
+  toggleExtraText() {
+    this.showFiller = !this.showFiller;
+    if (this.showFiller) {
+      // Adjust the sidenav's width when the extra text is shown
+      const sidenavElement = this.drawer.nativeElement;
+      sidenavElement.style.width = '400px';
+    } else {
+      // Reset the sidenav's width when the extra text is hidden
+      // Set the width to the original value (250px)
+      const sidenavElement = this.drawer.nativeElement;
+      sidenavElement.style.width = '250px';
+    }
+  }
 }
