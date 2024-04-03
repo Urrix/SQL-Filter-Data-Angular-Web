27575f13b2387f7a22c83d6464589a7b86abf0a8
KikeCrux <kikke8508@gmail.com>
2024-04-02 20:54:02 -0600

    Router test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { BusquedasComponent } from './busquedas/busquedas.component';
+import { FechaYHoraComponent } from './fecha-y-hora/fecha-y-hora.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'busquedas', component: BusquedasComponent
+    },
+    {
+        path: 'fecha', component: FechaYHoraComponent
+    }
+];
